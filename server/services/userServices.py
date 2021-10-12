b10cca33c0d289d497d436d164ea74e9067fea37
VerwinKwa@SMU <verwin.kwa.2019@sis.smu.edu.sg>
2021-10-13 01:32:25 +0800

    updated userServices and user.sql

--- server/services/userServices.py
+++ server/services/userServices.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:admin123@spm-database.cjmo3wwh5ar9.ap-southeast-1.rds.amazonaws.com:3306/spm_db'
@@ -21,7 +22,7 @@ class User(db.Model):
     userId = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(50))
     email = db.Column(db.String(50))
-    password = db.Column(db.String(50))
+    password = db.Column(db.String(120))
     userType = db.Column(db.String(50))
     
 
@@ -39,12 +40,16 @@ class User(db.Model):
         for column in columns:
             result[column] = getattr(self, column)
         return result
+    
+    def verify_password(self, password):
+        if check_password_hash(self.password, password):
+            return True
+        return False
 
 class Learner(User):
     __tablename__ = 'learner'
 
     userId = db.Column(db.Integer, db.ForeignKey('user.userId'), primary_key=True)
-
 
     __mapper_args__ = {
         'polymorphic_identity': 'learner',
@@ -55,7 +60,6 @@ class Trainer(User):
 
     userId = db.Column(db.Integer, db.ForeignKey('user.userId'), primary_key=True)
 
-
     __mapper_args__ = {
         'polymorphic_identity': 'trainer',
     }
@@ -65,7 +69,6 @@ class Hr(User):
 
     userId = db.Column(db.Integer, db.ForeignKey('user.userId'), primary_key=True)
 
-
     __mapper_args__ = {
         'polymorphic_identity': 'hr',
     }
@@ -73,9 +76,8 @@ class Hr(User):
 
 db.create_all()
 
-## TRAINER ROUTES ##
-# create trainer
-@app.route("/createTrainer", methods=['POST'])
+# create user
+@app.route("/createUser", methods=['POST'])
 def create_trainer():
     data = request.get_json()
     if not all(key in data.keys() for
@@ -89,19 +91,25 @@ def create_trainer():
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         return jsonify({
-            "message": "Trainer exists."
+            "message": "User exists."
         }), 404
-
-    trainer = Trainer(**data)
+    
+    if data['userType'] == 'Trainer':
+        new_user = Trainer(email=data['email'], userName=data['userName'], password=generate_password_hash(data['password'], method="sha256"), userType=data['userType'])
+    elif data['userType'] == 'Learner':
+        new_user = Learner(email=data['email'], userName=data['userName'], password=generate_password_hash(data['password'], method="sha256"), userType=data['userType'])
+    elif data['userType'] == 'HR':
+        new_user = Hr(email=data['email'], userName=data['userName'], password=generate_password_hash(data['password'], method="sha256"), userType=data['userType'])
     
     try:
-        db.session.add(trainer)
+        db.session.add(new_user)
         db.session.commit()
-        return jsonify(trainer.to_dict()), 201
+        return jsonify(new_user.to_dict()), 201
     except Exception:
         return jsonify({
             "message": "Unable to commit to database."
         }), 500
+
 
 # get trainer based on id or get all trainers
 @app.route("/getTrainers")
@@ -117,36 +125,6 @@ def trainers():
         }
     ), 200
 
-## LEARNER ROUTES ##
-# create learner
-@app.route("/createLearner", methods=['POST'])
-def create_learner():
-    data = request.get_json()
-    if not all(key in data.keys() for
-               key in ('userName', 'email',
-                       'password', 'userType')):
-        return jsonify({
-            "message": "Incorrect JSON object provided."
-        }), 500
-
-    user = User.query.filter_by(email=data['email']).first()
-
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
-        return jsonify({
-            "message": "Learner exists."
-        }), 404
-
-    learner = Learner(**data)
-    
-    try:
-        db.session.add(learner)
-        db.session.commit()
-        return jsonify(learner.to_dict()), 201
-    except Exception:
-        return jsonify({
-            "message": "Unable to commit to database."
-        }), 500
-
 # get learner based on id or get all learners
 @app.route("/getLearners")
 def learners():
@@ -161,36 +139,6 @@ def learners():
         }
     ), 200
 
-## HR ROUTES ##
-# create hr
-@app.route("/createHr", methods=['POST'])
-def create_hr():
-    data = request.get_json()
-    if not all(key in data.keys() for
-               key in ('userName', 'email',
-                       'password', 'userType')):
-        return jsonify({
-            "message": "Incorrect JSON object provided."
-        }), 500
-
-    user = User.query.filter_by(email=data['email']).first()
-
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
-        return jsonify({
-            "message": "HR exists."
-        }), 404
-
-    hr = Hr(**data)
-
-    try:
-        db.session.add(hr)
-        db.session.commit()
-        return jsonify(hr.to_dict()), 201
-    except Exception:
-        return jsonify({
-            "message": "Unable to commit to database."
-        }), 500
-
 # get hr based on id or get all hrs
 @app.route("/getHrs")
 def hrs():
@@ -204,53 +152,6 @@ def hrs():
             "data": [hr.to_dict() for hr in hr_list]
         }
     ), 200
-
-
-
-# create User Account
-# @app.route("/createUser", methods=['POST'])
-# def createUser():
-
-#     # retrieved data
-#     data = request.get_json()
-
-#     # check if proper data is sent
-#     if not all(key in data.keys() for
-#                key in ('userName, email', 'password', 'userType')):
-#         return jsonify({
-#             "message": "Incorrect JSON object provided."
-#         }), 500
-    
-#     user = User.query.filter_by(email=data['email']).first()
-
-#     if user: # if a user is found, we want to redirect back to signup page so user can try again
-#         return jsonify({
-#             "message": "User exists."
-#         }), 404
-    
-#     # creating id based on userType
-#     userType = data['userType']
-
-#     if userType == 'Learner':
-#         id = 'L-' + str(uuid.uuid4())
-#     elif userType == 'Trainer':
-#         id = 'T-' + str(uuid.uuid4())
-#     elif userType == 'HR':
-#         id = 'H-' + str(uuid.uuid4())
-
-#     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-#     new_user = User(id=id, email=data['email'], userName=data['userName'], password=generate_password_hash(data['password'], method='sha256'), userType=userType)
-    
-#     # add the new user to the database
-#     try:
-#         db.session.add(new_user)
-#         db.session.commit()
-#         return jsonify(new_user.to_dict()), 201
-#     except Exception:
-#         return jsonify({
-#             "message": "Unable to commit to database."
-#         }), 500
-
    
 # login to user account
 @app.route("/login", methods=['POST'])
@@ -267,26 +168,21 @@ def login():
         }), 500
     
     user = User.query.filter_by(email=data['email']).first()
-
+    
     # retrieve password
     password = data['password']
-
-    # check if user exists and validate password
-    # for hashed passwords
-    # if not user or not check_password_hash(user.password, password):
-    #     return jsonify({
-    #         "message": "Person not found."
-    #     }), 404
-    if not user or not user.password == password:
+    
+    if not user or not user.verify_password(password):
         return jsonify({
-            "message": "Person not found or incorrect password."
+            "message": "Person not found."
         }), 404
-
-    return jsonify(
-        {
-            "data": user.to_dict()
-        }
-    ), 200
+    
+    else:
+        return jsonify(
+            {
+                "data": user.to_dict()
+            }
+        ), 200
 
 
 if __name__ == '__main__':
