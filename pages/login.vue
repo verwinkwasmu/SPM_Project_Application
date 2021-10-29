b8f8b34278803ac989ee3474919596011a068c11
wayneeenggg <wayne.ng.2019@scis.smu.edu.sg>
2021-10-29 14:01:04 +0800

    login page

---
+++ pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <section id="contact" class="contact">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h2>Log IN</h2>
+          </div>
+          <div class="row">
+            <div class="col-lg-6" id="login">
+              <div id="app" class="">
+                    <div class="form-group">
+                      <label for="name">Username: </label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        name="subject"
+                        id="subject"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <button type="button" class="btn btn-primary">Login</button>
+                    </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+
+
+</script>
+
