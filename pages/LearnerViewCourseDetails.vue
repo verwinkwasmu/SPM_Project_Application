fbacd7af692e45088dbe2b0eb4ae10188e26f1a2
wayneeenggg <wayne.ng.2019@scis.smu.edu.sg>
2021-10-29 18:00:28 +0800

    Learner view course & view class

---
+++ pages/LearnerViewCourseDetails.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <Header/>
+
+        <!--<Homepage/>
+       -->
+        <section id="team" class="team section-bg">  <!--box-->
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-title">
+                    <h2>Fundamentals of Xerox WorkCentre 7845 <br></h2>
+                </div>
+                    <div class="row">
+                        <div class="col-lg-8" id="createCourse" style="padding-bottom: 50px">
+                            <div
+                                class="member d-flex align-items-start"
+                                data-aos="zoom-in"
+                                data-aos-delay="100"
+                            >
+                                <div class="LearnerEnrol">
+                                     <a href="LearnerViewClass" class="LearnerEnrol-btn">Enrol into course</a>
+                                </div>          
+                                <div class="member-info">
+                                <h4>Fundamentals of Xerox WorkCentre 7845</h4>
+                                Prerequisite Courses: 
+                                <ul>
+                                    <li>Fundamentals of Xerox</li>
+                                    <li>Fundamentals of Printing</li>
+                                </ul>
+                                
+                                <h4>Course Description: </h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </p>
+                                </div>
+                            </div>
+
+                        </div>     
+                    </div>
+            </div>
+
+        </section>
+    </div>
+</template>
