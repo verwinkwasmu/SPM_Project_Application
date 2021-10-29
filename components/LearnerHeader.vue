48684d6389783cc4d9706c9eac69e4fe1cf92380
wayneeenggg <wayne.ng.2019@scis.smu.edu.sg>
2021-10-29 17:09:46 +0800

    Learner view available courses
    - Learner Homepage
    - Learner View available courses

---
+++ components/LearnerHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <header id="header" class="fixed-top">
+      <div class="container d-flex align-items-center">
+        <a href="" class="logo me-auto"><img src="~/assets/img/LMS.png" alt="" class="img-fluid"> AIO Learning Management System </a>
+
+
+        <nav id="navbar" class="navbar">
+          <ul>
+            <li>
+                <a class="nav-link scrollto active" href="/">Home</a>
+            </li>
+            
+            <li>
+                <a class="nav-link" href="LearnerViewCourse">Enrol</a>
+            </li>
+            <li class="dropdown">
+              <a href="TrainerViewCourses"
+                ><span>My courses</span> <i class="bi bi-chevron-down"></i
+              ></a>
+              <ul>
+                <li><a href="#">Course 1</a></li>
+                <li><a href="#">Course 2</a></li>
+                <li><a href="#">Course 3</a></li>
+                <li><a href="#">Course 4</a></li>
+              </ul>
+            </li>
+
+            <li class="dropdown">
+              <a href="#"
+                ><span>User</span> <i class="bi bi-chevron-down"></i
+              ></a>
+              <ul>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Logout</a></li>
+              </ul>
+            </li>
+
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+      </div>
+    </header>
+</template>
---
+++ pages/LearnerViewCourse.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <LearnerHeader/>
+        <section id="services" class="services section-bg">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h2>Available Courses</h2>
+          </div>
+
+          <div class="row">
+            <div
+              class="col-xl-3 col-md-6 d-flex align-items-stretch"
+              data-aos="zoom-in"
+              data-aos-delay="100"
+            >
+              <div class="icon-box">
+                <div class="icon"><i class="bx bxl-dribbble"></i></div>
+                <h4><a href="">Fundamentals of Xerox WorkCentre 7845</a></h4>
+                <div class="course">
+                  <a href="LearnerViewClasses" class="course-btn">View Details</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </div>
+</template>
