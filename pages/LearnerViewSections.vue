736e1ce1c3365e71471eec0e79de4912b603dda2
wayneeenggg <wayne.ng.2019@scis.smu.edu.sg>
2021-10-31 18:17:40 +0800

    learner view section

---
+++ pages/LearnerViewSections.vue
@@ -0,0 +1,51 @@
+<!-- Please remove this file from your project -->
+<template>
+
+  <div>
+    <LearnerHeader/>
+    <main id="main">
+
+
+
+      <section id="team" class="team section-bg">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h2>Fundamentals of XXX</h2>
+            <p>
+            </p>
+          </div>
+            
+        </div>
+        <div class="sidenav">
+            <a href="#">Section 1</a>
+            <a href="#">Section 2</a>
+            <a href="#">Section 3</a>
+            <a href="#">Section 4</a>
+        </div>
+
+        <div class="content">
+            <h4>Section 1</h4>
+              <div
+                class="member d-flex align-items-start"
+                data-aos="zoom-in"
+                data-aos-delay="100"
+              >
+                <iframe src="~/assets/img/sample.pdf" width="100%" height="500px"></iframe>
+                
+              </div>
+              <br>
+               <div
+                class="member d-flex align-items-start"
+                data-aos="zoom-in"
+                data-aos-delay="100"
+              >
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/v64KOxKVLVg" allowfullscreen></iframe>
+                    </div>
+               </div>
+        </div>
+      </section>
+      
+    </main>
+  </div>
+</template>
