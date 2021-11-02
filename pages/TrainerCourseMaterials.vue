ab5b3acc39b40e605030ef1aa0299d670e6e6ef9
weixiangtoh <weixiangtoh982@gmail.com>
2021-11-02 15:35:40 +0800

    Create TrainerCourseMaterials.vue

---
+++ pages/TrainerCourseMaterials.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <TrainerHeader/>
+
+        <section id="team" class="team section-bg">
+            <div class="container" data-aos="fade-up">
+                <div class="section-title">
+                  
+                    <h2>Fundamentals of Xerox WorkCentre 7845</h2>
+                    <div class="TrainerCreateQuiz">
+                        <a href="TrainerCreateQuiz" class="TrainerCreateQuiz-btn">Create Quiz</a>
+                    </div>
+
+                </div>
+                   
+                  <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">Current Quizzes</th>
+                        <th scope="col">Quiz Type</th>
+                        <th scope="col">View Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td> Assignment 1</td>  <!-- can click onto it to see the quiz questions--> 
+                        <td>Graded</td>
+                        <td>Released</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </section>
+    </div>
+</template>
+
+<script>
+
+</script>  
