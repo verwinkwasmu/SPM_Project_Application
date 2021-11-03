b99abca50098183ce86fbb50498defd99a11d1bc
weixiangtoh <weixiangtoh982@gmail.com>
2021-11-03 19:20:52 +0800

    Merge branch 'dev' into SPM-171

--- pages/LearnerTakeQuiz.vue
+++ pages/LearnerTakeQuiz.vue
@@ -3,15 +3,17 @@
       <TrainerHeader/>
       <section id="team" class="team section-bg">
           <div class="" data-aos="fade-up">
-              <div id='App' class="container" data-aos="fade-up">
+              <div id='App' class="container">
                 <div class="section-title">
-                  <!-- <h2>{{courseName}}</h2> -->
                   <h2>{{courseName}}</h2>
                   <h3> Section 1 Quiz </h3>
                 </div>
-                  
+                        <div class="timer">Time Remaining: <strong>{{ displayTime }} </strong></div>
+
                           <div class="" v-for="(question, index) in questions" :key="index">
                               <h4>{{index+1}}. {{question.fullquestion}}</h4>
+                              <br>
+                              
                               <div class="optionContainer">
                                       <div class="option">
                                           <label id="quizrbtn"><input type="radio" name="optradio">    
@@ -32,14 +34,13 @@
                                           <label id="quizrbtn"><input type="radio" name="optradio" :class="'is-selected'">     
                                           {{question.option4}}</label>
                                       </div>
-                                      <div id="remainingTime">
-                                        hihihihi {{timer}}
-                                      </div>
+
+
+                                    
                               </div>
                               <hr>
                           </div>
-                  <!-- </div>
-              </div>    -->
+          
               </div>
       
               </div>
@@ -59,6 +60,8 @@
     loading: false,
     error: null,
     courseName: 'Fundamentals of XXX',
+    time: 1800,
+    displayTime: '',
 
     questions: [
       {
@@ -68,8 +71,28 @@
         option3: "Maybe",
         option4: "Maybe not",
       }],
-
-    quizTimer: 30,
   }),
-  };
+
+    methods: {
+      countdown() {
+        this.time--;
+        let minuteTime = parseInt(this.time / 60, 10);
+        let secondTime = parseInt(this.time % 60, 10);
+        this.displayTime = minuteTime + " minutes " + secondTime + " seconds";
+        if (this.time === 0) {
+          clearInterval(this.timer);
+        }
+    },
+  },
+
+  mounted() {
+      this.timer = setInterval(this.countdown, 1000);
+    },
+
+  beforeDestroy() {
+      clearInterval(this.timer);
+
+    },
+
+};
 </script>
--- pages/TrainerQuizList.vue
+++ pages/TrainerQuizList.vue
@@ -4,11 +4,16 @@
 
         <section id="team" class="team section-bg">
             <div class="container" data-aos="fade-up">
+                <div class="row pb-5 mb-2 ml-0">
+                    <div class="viewClass">
+                        <router-link :to="{path: '/TrainerViewSection', query: {classId: classObj.classId}}" class="viewClass-btn">Back to see all Classes</router-link>
+                    </div>
+                </div>
                 <div class="section-title">
                   
-                    <h2>Fundamentals of Xerox WorkCentre 7845</h2>
-                    <div class="TrainerCreateQuiz">
-                        <a href="TrainerCreateQuiz" class="TrainerCreateQuiz-btn">Create Quiz</a>
+                    <h2>{{course.courseName}}</h2>
+                    <div class="TrainerCreateSection">
+                        <router-link :to="{path: '/TrainerCreateQuiz', query: {sectionId: sectionId, classId: classObj.classId}}"  class="TrainerCreateSection-btn">Create Quiz</router-link>
                     </div>
 
                 </div>
@@ -36,5 +41,42 @@
 </template>
 
 <script>
+import axios from "axios";
 
-</script>  
+export default {
+  data: () => ({
+    course: {},
+    sectionId: "",
+    classObj: {},
+    error: false,
+    message: "",
+    enrolment: {},
+    courseId: localStorage.getItem('courseId')
+  }),
+  async mounted() {
+    // const apiUrl1 = `http://localhost:5002/viewSections/${this.$route.query.classId}`;
+    const apiUrl1 = `http://localhost:5002/getCourse/${this.courseId}`;
+    const apiUrl2 = `http://localhost:5002/getClass/${this.$route.query.classId}`;
+    try {
+      let response1 = await axios.get(apiUrl1);
+      let response2 = await axios.get(apiUrl2);
+
+      this.course = await response1.data;
+      this.classObj = await response2.data;
+      this.sectionId = await this.$route.query.sectionId;
+  
+      console.log(this.classObj);
+      console.log(this.course);
+      console.log(this.sectionId);
+      
+
+      this.error = false;
+    } catch (err) {
+      console.log(err);
+      this.error = true;
+      this.message = err;
+    }
+  },
+
+};
+</script>
