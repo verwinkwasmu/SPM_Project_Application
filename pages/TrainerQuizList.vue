d675ced999fa140446e4e52e061d0ac223eed9cd
weixiangtoh <weixiangtoh982@gmail.com>
2021-11-03 22:42:13 +0800

    Create Trainer View Quiz and Questions

--- pages/TrainerQuizList.vue
+++ pages/TrainerQuizList.vue
@@ -11,9 +11,31 @@
                 </div>
                 <div class="section-title">
                   
-                    <h2>{{course.courseName}}</h2>
+                    <h2>{{sectionId}}</h2>
+                    <h3>{{course.courseName}}</h3>
+                    <div class="row pt-3">
+                      <div class="col-lg-6" id="TrainerViewCourseDetails" style="padding-bottom: 40px">
+                          <div class="member d-flex"
+                                    data-aos="zoom-in"
+                                    data-aos-delay="100">      
+                              <div v-if="quizExist==true" class="member-info mx-auto">
+                                <h4>{{ quiz.quizId }}</h4> 
+                                <br>
+                                <h4>Quiz Time: {{quiz.time}}mins</h4>
+                              </div>
+                              <div v-else class="member-info mx-auto">
+                                <h4 class="text-danger">No Quiz Created yet</h4> 
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+
+
                     <div class="TrainerCreateSection">
-                        <router-link :to="{path: '/TrainerCreateQuiz', query: {sectionId: sectionId, classId: classObj.classId}}"  class="TrainerCreateSection-btn">Create Quiz</router-link>
+                        <router-link :to="{path: '/TrainerCreateQuiz', query: {sectionId: sectionId, classId: classObj.classId}}"  class="TrainerCreateSection-btn">
+                        <span v-if="quizExist==false">Create Quiz</span>
+                        <span v-else>Add Questions</span>
+                        </router-link>
                     </div>
 
                 </div>
@@ -21,16 +43,26 @@
                   <table class="table">
                     <thead>
                         <tr>
-                        <th scope="col">Current Quizzes</th>
-                        <th scope="col">Quiz Type</th>
-                        <th scope="col">View Status</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Question</th>
+                        <th scope="col">Option 1</th>
+                        <th scope="col">Option 2</th>
+                        <th scope="col">Option 3</th>
+                        <th scope="col">Option 4</th>
+                        <th scope="col">Answer</th>
+                        <th scope="col">Explanation</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <td> Assignment 1</td>  <!-- can click onto it to see the quiz questions--> 
-                        <td>Graded</td>
-                        <td>Released</td>
+                        <tr v-for="question in questions" :key="question.questionId">
+                          <td>{{question.questionId}}</td>
+                          <td>{{question.question}}</td>
+                          <td>{{question.option[0]}}</td>
+                          <td>{{question.option[1]}}</td>
+                          <td>{{question.option[2]}}</td>
+                          <td>{{question.option[3]}}</td>
+                          <td>{{question.answer}}</td>
+                          <td>{{question.explanation}}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -51,15 +83,27 @@ export default {
     error: false,
     message: "",
     enrolment: {},
+    quiz:{},
+    questions: [],
+    quizExist: false,
     courseId: localStorage.getItem('courseId')
   }),
   async mounted() {
-    // const apiUrl1 = `http://localhost:5002/viewSections/${this.$route.query.classId}`;
     const apiUrl1 = `http://localhost:5002/getCourse/${this.courseId}`;
     const apiUrl2 = `http://localhost:5002/getClass/${this.$route.query.classId}`;
+    const apiUrl3 = `http://localhost:5003/quiz/${this.$route.query.classId}/${this.$route.query.sectionId}`;
     try {
       let response1 = await axios.get(apiUrl1);
       let response2 = await axios.get(apiUrl2);
+      let response3 = await axios.get(apiUrl3);
+
+      console.log(response3.data);
+      if (response3.status == 200){
+        this.quiz = await response3.data.quiz;
+        this.questions = await response3.data.questions;
+        console.log(this.questions);
+        this.quizExist = true;
+      }
 
       this.course = await response1.data;
       this.classObj = await response2.data;
