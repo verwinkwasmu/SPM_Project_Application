902cf49a501d42bd8abcc4d6e9f2c4d1edef1690
weixiangtoh <weixiangtoh982@gmail.com>
2021-11-04 18:59:44 +0800

    Add Questions to Quiz

--- pages/TrainerQuizList.vue
+++ pages/TrainerQuizList.vue
@@ -43,7 +43,6 @@
                   <table v-if="quizExist==true" class="table">
                     <thead>
                         <tr>
-                        <th scope="col">#</th>
                         <th scope="col">Question</th>
                         <th scope="col">Option 1</th>
                         <th scope="col">Option 2</th>
@@ -51,18 +50,22 @@
                         <th scope="col">Option 4</th>
                         <th scope="col">Answer</th>
                         <th scope="col">Explanation</th>
+                        <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr v-for="question in questions" :key="question.questionId">
-                          <td>{{question.questionId}}</td>
                           <td>{{question.question}}</td>
-                          <td>{{question.option[0]}}</td>
-                          <td>{{question.option[1]}}</td>
-                          <td>{{question.option[2]}}</td>
-                          <td>{{question.option[3]}}</td>
+                          <td v-for="option in question.option" :key="option">
+                            {{option}}
+                          </td>
                           <td>{{question.answer}}</td>
                           <td>{{question.explanation}}</td>
+                          <td>
+                            <div class="form-group" id="removequestion">
+                              <button @click="removeQn(question.questionId)" type="button" class="btn btn-danger">Remove</button>
+                            </div> 
+                          </td>
                         </tr>
                     </tbody>
                 </table>
@@ -92,33 +95,56 @@ export default {
     const apiUrl1 = `http://localhost:5002/getCourse/${this.courseId}`;
     const apiUrl2 = `http://localhost:5002/getClass/${this.$route.query.classId}`;
     const apiUrl3 = `http://localhost:5003/quiz/${this.$route.query.classId}/${this.$route.query.sectionId}`;
-    try {
-      let response1 = await axios.get(apiUrl1);
-      let response2 = await axios.get(apiUrl2);
-      let response3 = await axios.get(apiUrl3);
+    
+    let response1 = await axios.get(apiUrl1);
+    let response2 = await axios.get(apiUrl2);
+    let response3 = await axios.get(apiUrl3);
 
-      console.log(response3.data);
-      if (response3.status == 200){
-        this.quiz = await response3.data.quiz;
-        this.questions = await response3.data.questions;
-        console.log(this.questions);
-        this.quizExist = true;
+    console.log(response3.data);
+    if (response3.status == 200){
+      this.quiz = await response3.data.quiz;
+      this.questions = await response3.data.questions;
+      console.log(this.questions);
+      this.quizExist = true;
+    }
+
+    this.course = await response1.data;
+    this.classObj = await response2.data;
+    this.sectionId = await this.$route.query.sectionId;
+
+    console.log(this.classObj);
+    console.log(this.course);
+    console.log(this.sectionId);
+    
+
+    this.error = false;
+    
+  },
+  methods:{
+    async removeQn(questionId){
+      const apiUrl = `http://localhost:5003/removeQuestion`;
+      const data = {
+        sectionId: this.sectionId,
+        classId: this.classObj.classId,
+        quizId: this.quiz.quizId,
+        questionId: questionId
+      };
+      try {
+        let response = await axios.delete(apiUrl, { data: data });
+        if (response.status == 200){
+          alert(response.data.message);
+          window.location.reload(true);
+        }
+        else{
+          alert("Please Try again!");
+        }
+
+        this.error = false;
+      } catch (err) {
+        console.log(err);
+        this.error = true;
+        this.message = err;
       }
-
-      this.course = await response1.data;
-      this.classObj = await response2.data;
-      this.sectionId = await this.$route.query.sectionId;
-  
-      console.log(this.classObj);
-      console.log(this.course);
-      console.log(this.sectionId);
-      
-
-      this.error = false;
-    } catch (err) {
-      console.log(err);
-      this.error = true;
-      this.message = err;
     }
   },
 
