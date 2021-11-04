e3e1be3ff0ab3beadf221fe17c2cb4420b1b5de3
wayneeenggg <wayne.ng.2019@scis.smu.edu.sg>
2021-11-04 16:44:01 +0800

    Create Learner View Badges

---
+++ pages/LearnerViewAllBadges.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <LearnerHeader/>
+        <section id="services" class="services section-bg">
+        <div class="container" data-aos="fade-up">
+          <!-- <div class="section-title">
+            <h2>My Completed Courses</h2>
+          </div>
+          <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Course ID</th>
+                <th scope="col">Course Name</th>
+                <th scope="col">Completed Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>B-101</td>
+                <td>Understanding the Brother 225</td>
+                <td>10/11/2020</td>
+                </tr>
+                <tr>
+                <td>B-102</td>
+                <td>Common Fixes for Brother 225</td>
+                <td>09/10/2020</td>
+                </tr>
+            </tbody>
+          </table> -->
+          <div class="section-title">
+            <h2>My Badges</h2>
+          </div>
+            <div class="card" style="width: 18rem;">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        </section>
+
+    </div>
+</template>
