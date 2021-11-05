a69b5830a049418c43984e43aa6c03dcf6aee061
VerwinKwa@SMU <verwin.kwa.2019@sis.smu.edu.sg>
2021-11-05 15:08:06 +0800

    sql file modifications

--- server/database/course.sql
+++ server/database/course.sql
@@ -29,8 +29,13 @@ CREATE TABLE `course` (
 --
 
 INSERT INTO `course` (`courseId`, `courseName`, `courseDescription`, `prerequisites`) VALUES
-('XRX-101', 'hello', 'ur mother', 'coursename'),
-('XRX-102', 'hello2', 'ur father', 'coursename');
+('B-102', 'Common Fixes for Brother 225', 'Finish this course to understand the common problems of Brother 225 that our customers usually bring up along with the f', null),
+('HP-111', 'Cleaning the HP 619', 'This course highlights the techniques required to clean the HP 619 Printer thoroughly.', null),
+('HP-112', 'Boosting the HP 619 Performance', 'This course perfectly highlights the nuances and ways to increase the performance of the HP 619', null),
+('XRX-101', 'Fundamentals of Xerox Work Centre 7845', 'An introductory course to the fundamentals of Xerox', null),
+('XRX-102', 'Advanced Xerox Work Centre 7900', 'An advanced course on Xerox Work Centre 7900', 'XRX-101:Fundamentals of Xerox Work Centre 7845'),
+('XRX-103', 'Final Xerox Work Centre 7900', 'The final extensive course on Xerox Work Centre 7900', 'XRX-102:Advanced Xerox Work Centre 7900'),
+('XRX-201', 'Programming for Xerox Work Centre with Card Access and Integration', 'A programming course on Xerox Work Centre', null);
 
 ALTER TABLE `course`
   ADD PRIMARY KEY (`courseId`);
