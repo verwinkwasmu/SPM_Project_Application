b4cc70f55defe1a8af265c67f7cba39d4ef25fc6
VerwinKwa@SMU <verwin.kwa.2019@sis.smu.edu.sg>
2021-11-05 20:36:23 +0800

    Added nicer alert notification for completed material alert

---
+++ components/Modal.vue
@@ -0,0 +1,15 @@
+<template>
+    <b-modal id="bv-modal-example" hide-footer> 
+      <template #modal-title> <img src="../assets/img/LMS.png" class="rounded me-2" alt="" width="10%"/>AIO Alerts </template>
+      <div class="d-block text-center">
+        <h3>{{message}}</h3>
+      </div>
+    </b-modal>
+</template>
+
+<script>
+export default {
+    props: ['message']
+}
+</script>
+
